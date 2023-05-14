7c93adda8e07c994f19c4476151e60ded85d1d45
Maniraho <johmaniraho42@gmail.com>
2023-05-14 10:50:41 +0300

    Count, Summ in loops

--- Iterations More Patterns/counting_loop.py
+++ Iterations More Patterns/counting_loop.py
@@ -1,1 +1,24 @@
+# counting in loop
+
+zork = 0
+print("Before", zork)
+for thing in [9, 41, 12, 3, 74, 15]:
+    zork = zork + 1
+    print(zork, thing)
+print("After", zork)
+
+print("\n")
+
+# Summing in loop
+
+other_zork = 0
+for other_thing in [9, 41, 12, 3, 74, 15]:
+    other_zork = other_zork + other_thing
+    print(other_zork, other_thing)
+print("After", other_zork)
+
+"""
+To add a value we encounter in loop, we introduce a sum variable that
+starts at 0 and we add the value to the sum each time through the loop  
+"""
 
