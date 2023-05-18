5469013e3d51887d3d35546546478994f89d41f9
Maniraho <johmaniraho42@gmail.com>
2023-05-18 10:48:13 +0300

    Search and Replace

--- Intermediate Strings/string_search.py
+++ Intermediate Strings/string_search.py
@@ -1,1 +1,24 @@
+# Search and Replace
 
+greet = 'Hello Bob'
+nstr = greet.replace('Bob', 'Jane')
+print(nstr)
+
+line = 'Please have a good day!'
+print(line.startswith('Please'))
+print(line.startswith('p'))
+
+data = 'From stephen.marquard@uct.ac.za Sat June 5 09:14:16 2008'
+atpos = data.find('@')
+print(atpos)
+
+sppos = data.find(' ', atpos)
+print(sppos)
+
+host = data[atpos+1: sppos]
+print(host)
+
+# challenge
+word = "bananana"
+i = word.find("na")
+print(i)
