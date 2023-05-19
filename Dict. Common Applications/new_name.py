36c2e24956cee4ca2970a591e92b6f5d9e29e3b9
Maniraho <johmaniraho42@gmail.com>
2023-05-19 08:50:36 +0300

    Search and Replace

--- Dict. Common Applications/new_name.py
+++ Dict. Common Applications/new_name.py
@@ -1,1 +1,15 @@
+# when we see a new name
+counts = dict()
+names = ['csev', 'cwen', 'csev', 'zquian', 'cwen']
+for name in names:
+    if name not in counts:
+        counts[name] = 1
+    else:
+        counts[name] = counts[name]+1
+print(counts)
 
+"""
+When we encounter a new name; we need to add a new entry in the dictionary
+and if this the second or later we have seen the name, we simply add one to the count
+in the dictionary under that name.
+"""
