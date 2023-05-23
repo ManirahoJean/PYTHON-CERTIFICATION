0411d2ab39aeecdfbbca3cf5863accc0f52ea422
Maniraho <johmaniraho42@gmail.com>
2023-05-23 19:17:39 +0300

    Counting pattern

--- Dictionaries and Loops/counting_pattern.py
+++ Dictionaries and Loops/counting_pattern.py
@@ -1,1 +1,30 @@
+# Counting pattern
+"""
+the clown ran after the car and the car run into the tent
+and the tent fell down on the clown and the car
+"""
 
+counts = dict()
+print("Enter a line of text: ")
+line = input('')
+words = line.split()
+print("Words: ", words)
+print("counting...")
+
+for word in words:
+    counts[word] = counts.get(word, 0) + 1
+print('counts', counts)
+
+"""
+The general pattern t0 count the words in a line of text is to 
+split the line into words and use a dictionary to track the count of
+each word independently
+"""
+
+# Question
+# What will the following code print?
+
+counts = {'chuck': 1, 'annie': 42, 'jan': 100}
+for key in counts:
+    if counts[key] > 10:
+        print(key, counts[key])
