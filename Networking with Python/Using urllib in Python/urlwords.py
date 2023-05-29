4c55768ab08e6a4ab1c4b0c402d7e812a40742b8
Maniraho <johmaniraho42@gmail.com>
2023-05-29 09:19:28 +0300

    Reading Web Pages

--- Networking with Python/Using urllib in Python/urlwords.py
+++ Networking with Python/Using urllib in Python/urlwords.py
@@ -4,14 +4,6 @@ Since HTTP is so common, we have a library that does all the
 socket work for us and makes web pages look like a file
 """
 
-"""
-import urllib.request, urllib.parse, urllib.error
-
-fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.text')
-for line in fhand:
-    print(line.decode().strip())
-"""
-
 import urllib.request, urllib.parse, urllib.error
 
 fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
