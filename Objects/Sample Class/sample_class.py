b9a0bf23569edef3c20ce93bbc2c193b94e36508
Maniraho <johmaniraho42@gmail.com>
2023-06-03 20:18:12 +0300

    Sample Class

--- Objects/Sample Class/sample_class.py
+++ Objects/Sample Class/sample_class.py
@@ -1,1 +1,15 @@
 
+# Objects: A Sample Class
+
+class PartyAnimal:
+    x = 0
+
+    def party(self):
+        self.x = self.x + 1
+        print('so far', self.x)
+
+
+an = PartyAnimal()
+an.party()
+an.party()
+an.party()
