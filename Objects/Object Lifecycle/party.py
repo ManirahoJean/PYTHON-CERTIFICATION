7ba74f62b2802c4961c70683933949603ba3d9b5
Maniraho <johmaniraho42@gmail.com>
2023-06-07 08:44:41 +0300

    Object Lifecycle, constructed,destructed

--- Objects/Object Lifecycle/party.py
+++ Objects/Object Lifecycle/party.py
@@ -1,1 +1,20 @@
+# Object lifecycle
+class PartyAnimal:
+    x = 0
+    
+    def __init__(self):
+        print('I am contructed')
 
+    def party(self):
+        self.x = self.x + 1
+        print('so far', self.x)
+
+    def __del__(self):
+        print('I am destructed', self.x)
+
+
+an = PartyAnimal()
+an.party()
+an.party()
+an = 42
+print('an contains', an)
